e5c8422151a345be5440b8873818d759eb3be0d9
Aayushi <aayushi@aayushi-VirtualBox>
2018-09-11 23:57:46 +0530

    KBC code

---
+++ KBC_Assignment.c
@@ -0,0 +1,82 @@
+#include<stdio.h>
+
+int main()
+{
+
+	printf("\t\t\tWelcome to KBC 2018\n");
+	printf("Q1) The International Literacy Day is observed on? \n");
+	printf(" 1.Sep 8 \t 2.Nov 28 \t 3.2 May \t 4.Sep22 \n"); 
+	int ans;
+	
+        scanf("%d",&ans);
+	if (ans==1)
+	{
+		printf("You won Rs. 5000\n");
+		printf("\n Q2) Who was the lead actress in the film Padmavat? \n");
+	  	printf(" 1.Rani Mukherji \t 2.Anushka Sharma \t 3.Deepika Padukone \t 4.Katrina 		Kaif \n");
+	  	
+	 scanf("%d", &ans);
+	 if (ans==3)
+	 {
+		printf("You won Rs. 10,000\n");
+		printf("\n Q3)  Bahubali festival is related to \n");
+	  	printf(" 1.Islam \t 2.Hinduism \t 3.Buddhism \t 4.Jainism \n");
+
+	  scanf("%d", &ans);
+	  if (ans==4)
+	 {
+		printf("You won Rs. 20,000\n");
+		printf("\n Q4) The South-West coast-Goa part of India is popularly Known as? \n");
+	  	printf(" 1.Coromandel \t 2.Malabar \t 3.Amritsar \t 4.Konkan \n");
+
+	  scanf("%d", &ans);
+	  if (ans==4)
+	 {
+		printf("You won Rs.40,000\n");
+		printf("Q5) Which one these politians became the president of his party in 			december 2017?\n");
+	        printf(" 1.K Chandrashekhar Rao \t 2.Raj Thackeray \t 3.Rahul Gandhi \t 4.Naveen 			Patnaik\n");
+
+	  scanf("%d", &ans);
+	  if (ans==3)
+	 {
+		printf("You won Rs.80,000\n");
+		printf("\n Q6) Recently which car was introduced by Tata motors during auto expo 			2018 in India? \n");
+	        printf(" 1.H5X and 45X \t 2.Tigor \t 3.Tiago \t 4.Ertiga\n");
+	
+          scanf("%d", &ans);
+	  if (ans==1)
+	 {
+		printf("You won RS.3,20,000\n");
+		printf("\n Q7) Who among these is regarded as the inventor of the air 			conditioners? \n");
+	  	printf(" 1.Henry Ford \t 2.Willis Carrier \t 3.Issac Singer \t 4.Henning 			Larsen\n");
+	  					
+          scanf("%d", &ans);
+	  if (ans==2)
+	 {
+		printf("You won Rs.6,40,000\n");
+	        printf("Q8) Whos is the Chief Minister of Delhi in the year 2015? \n");
+	  	printf(" 1.Arvind Kejriwal \t 2.Devendra Fadnavis \t 3.Manohar Parrikar \t 			4.Siddaramaiah\n");
+	  							
+          scanf("%d", &ans);
+	  if (ans==1)
+         {
+		printf("You won Rs.12,50,000\n");
+	        printf("\n Q9) Which theoritical physicist passed away recently? \n");
+                printf(" 1.Issac Newton \t 2.Dalhousie \t 3.Jr Claudia \t 4.Stephen Hawking\n");
+	  								
+          scanf("%d", &ans);
+	  if (ans==4)
+	 {
+	        printf("You won Rs.25,00,000\n");
+	        printf("\n Q10) Sardar Sarovar is built on which river? \n");
+	        printf(" 1.Narmada \t 2.Tapi \t 3.Mahi \t 4.Chambal\n");
+	  
+     	  scanf("%d", &ans);
+	  if (ans==1)
+	{
+		printf("You won Rs.50,00,000\n");
+									}
+							
+	
+	return 0;
+  }
